07ce9242270e9c3c7065012f6c5f3dbc96c5c148
freelancing-solutions <mobiusndou@gmail.com>
2023-02-23 18:15:16 +0200

    updated the API Keys Model to work with rate limiting of the API GateWay

--- src/databases/models/sql/api_keys.py
+++ src/databases/models/sql/api_keys.py
@@ -15,9 +15,16 @@ class APIKeys(Base):
     uuid: str = Column(String(UUID_LEN), index=True)
     api_key: str = Column(String(API_KEY_LEN), primary_key=True, index=True)
     subscription_id: str = Column(String(UUID_LEN))
+    duration: int = Column(Integer)
+    limit: int = Column(Integer)
+    last_request_timestamp: float = Column(Float)
+    requests_count: int = Column(Integer)
 
     def to_dict(self) -> dict[str, str]:
         return {
             "uuid": self.uuid,
             "api_key": self.api_key,
-            "subscription_id": self.subscription_id}
+            "subscription_id": self.subscription_id,
+            "duration": self.duration,
+            "last_request_timestamp": self.last_request_timestamp,
+            "requests_count": self.requests_count}
