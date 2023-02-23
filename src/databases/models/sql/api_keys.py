dbff032e34811859432f40888fe7ee1e673b8f15
freelancing-solutions <mobiusndou@gmail.com>
2023-02-23 18:30:57 +0200

    updated api keys model

--- src/databases/models/sql/api_keys.py
+++ src/databases/models/sql/api_keys.py
@@ -1,24 +1,20 @@
+from sqlalchemy import Column, String, Integer, Float, Boolean
 
-from datetime import datetime
-from typing import Self
-
-from sqlalchemy import Column, String, inspect, Integer, Float
-from src.databases.const import UUID_LEN, NAME_LEN, EMAIL_LEN, STR_LEN, CELL_LEN, API_KEY_LEN
-from src.databases.models.sql import Base, mysql_instance, sessionType
-from src.utils import date_from_timestamp
+from src.databases.const import UUID_LEN, API_KEY_LEN
+from src.databases.models.sql import Base
 
 
 class APIKeys(Base):
     """
 
     """
+    __tablename__ = "eod_api_keys"
     uuid: str = Column(String(UUID_LEN), index=True)
     api_key: str = Column(String(API_KEY_LEN), primary_key=True, index=True)
     subscription_id: str = Column(String(UUID_LEN))
     duration: int = Column(Integer)
     limit: int = Column(Integer)
-    last_request_timestamp: float = Column(Float)
-    requests_count: int = Column(Integer)
+    is_active: bool = Column(Boolean, default=True)
 
     def to_dict(self) -> dict[str, str]:
         return {
@@ -26,5 +22,4 @@ class APIKeys(Base):
             "api_key": self.api_key,
             "subscription_id": self.subscription_id,
             "duration": self.duration,
-            "last_request_timestamp": self.last_request_timestamp,
-            "requests_count": self.requests_count}
+            "limit": self.limit}
