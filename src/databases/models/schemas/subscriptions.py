bad2c863237d9ca040034c59ae2310398bbde605
freelancing-solutions <mobiusndou@gmail.com>
2023-03-13 18:15:56 +0200

    added Schemas for user data

--- src/databases/models/schemas/subscriptions.py
+++ src/databases/models/schemas/subscriptions.py
@@ -3,8 +3,9 @@ from pydantic import BaseModel
 
 
 class SubscriptionModel(BaseModel):
-    subscription_id: str
-    plan_id: str
+    """Basic Subscription Model Schema - can be used to create new schema, update a schema and get schema data"""
+    subscription_id: str | None
+    plan_id: str | None
     uuid: str | None = None
     time_subscribed: float | None = None
     payment_day: str | None = None
