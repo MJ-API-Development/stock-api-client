62cdca80ad4babe1ca0d633bd0251582a78f6450
freelancing-solutions <mobiusndou@gmail.com>
2023-03-30 14:52:49 +0200

    adding token based authentication

--- src/static/js/sw.js
+++ src/static/js/sw.js
@@ -1,39 +1,63 @@
 self.addEventListener('fetch', event => {
-  event.respondWith(addAuthHeader(event.request));
+  /**
+   * This code sets up an event listener for the 'fetch' event, which is fired whenever the browser makes
+   * a fetch request. When the event is triggered, it calls the handle_authentication() function to handle the
+   * request.
+   * The event.respondWith() method is used to respond to the fetch request with the result of the
+   * handle_authentication() function. This ensures that the response is modified with the X-Auth-Token header
+   * before it is returned to the browser.
+   */
+  event.respondWith(handle_authentication(event.request));
 });
 
-async function addAuthHeader(request) {
-  const token = await getUserToken();
+async function handle_authentication(request) {
+  /**
+   * This function takes a request object as input and adds an X-Auth-Token header to it. It then creates a
+   * new request object with the modified headers and uses it to fetch the requested resource.
+   * If the response includes an X-Auth-Token header, the token is extracted and saved in the cache
+   * using the save_user_token() function. Finally, the response is returned.
+   * @type {Cache}
+   */
+  const cache = await caches.open('eod');
+  const token = await get_user_token(cache);
 
   const headers = new Headers(request.headers);
   headers.append('X-Auth-Token', token);
 
-  const modifiedRequest = new Request(request, {
+  const service_request = new Request(request, {
     headers: headers,
     mode: 'cors', // or 'no-cors' if you're requesting a resource from another origin
-    cache: 'default' // or 'no-cache' if you don't want the response to be cached
+    cache: 'no-cache' // or 'no-cache' if you don't want the response to be cached
   });
 
-  const cache = await caches.open('eod');
-  let response = await fetch(modifiedRequest);
+  let response = await fetch(service_request);
 
   if (response) {
     if (response.ok && response.headers.get('X-Auth-Token')) {
       const token = response.headers.get('X-Auth-Token');
-      await saveUserToken(token);
+      await save_user_token(token, cache);
     }
   }
+
   return response;
+
 }
 
-async function getUserToken() {
-  const cache = await caches.open('my-cache');
+async function get_user_token(cache) {
+  /**
+   * This function retrieves a user token from the specified cache (named "eod" in this case) and returns it.
+   * If the token is not found in the cache, it returns an empty string.
+   * @param {Cache} cache
+   * @type {*}
+   */
   const token = await cache.match('token');
   return token ? token.text() : '';
 }
 
-async function saveUserToken(token) {
-  const cache = await caches.open('my-cache');
-
+async function save_user_token(token, cache) {
+  /**
+   * This function saves the specified token in the specified cache (named "eod" in this case) using the put() method.
+   * It returns a Promise that resolves when the token has been saved in the cache.
+   */
   return cache.put('token', new Response(token));
 }
