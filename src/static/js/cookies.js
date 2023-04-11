3cd468ba29c2aa3aa3a9982380ce2f010197bf7f
freelancing-solutions <mobiusndou@gmail.com>
2023-04-11 12:51:57 +0200

    Fixed Cookies

--- src/static/js/cookies.js
+++ src/static/js/cookies.js
@@ -1,49 +1,57 @@
-self.addEventListener('load', async () => {
-    if (document.cookie.indexOf("cookie-accepted=true") !== -1) {
-      // Cookie has been accepted, don't display pop-up
-      document.getElementById("cookie-popup").style.display = "none";
-    } else {
-      // Cookie has not been accepted, display pop-up
-      document.getElementById("cookie-popup").style.display = "block";
-    }
 
-})
+const cookieName = 'cookie-accepted';
 
-// set the time in milliseconds
-const popupTimeout = 45000; // 45 seconds
+// document.addEventListener('load', async () => {
+//     if (document.cookie.indexOf("cookie-accepted=true") !== -1) {
+//       // Cookie has been accepted, don't display pop-up
+//       document.getElementById("cookie-popup").style.display = "none";
+//     } else {
+//       // Cookie has not been accepted, display pop-up
+//       document.getElementById("cookie-popup").style.display = "block";
+//     }
+// });
 
-// function to remove the cookie pop-up
 function removePopup() {
-  const popup = document.getElementById("cookie-popup");
-  if (popup) {
-    popup.remove();
-    // set a cookie to remember that the user has accepted the cookie policy
-    document.cookie = "cookie-accepted=true; path=/";
+    const popup = document.getElementById("cookie-popup");
+    if (popup) {
+      popup.remove();
+      // set a cookie to remember that the user has accepted the cookie policy
+      setCookie(cookieName, 'true', 365);
+    }
+}
+
+//    will ensure that the popup is removed if not clicked and an assumption that the cookie is accepted
+(function() {
+  // set the time in milliseconds
+  const popupTimeout = 30000; // 30 seconds
+  // remove the cookie pop-up after a certain amount of time
+  setTimeout(removePopup, popupTimeout);
+})();
+
+
+
+(function() {
+  // Get the cookie popup element
+  const cookiePopup = document.getElementById('cookie-popup');
+  // Get the cookie accept button
+  const cookieAccept = document.getElementById('cookie-accept');
+  // Check if the cookie is set
+  if (getCookie(cookieName) !== true) {
+      // Show the cookie popup
+      cookiePopup.style.display = 'none';
+  } else {
+      cookiePopup.style.display = 'block';
   }
-}
+  // Add a click event listener to the cookie accept button
+  cookieAccept.addEventListener('click', function() {
+      // Set the cookie to expire in 365 days
+      setCookie(cookieName, 'true', 365);
+      // Hide the cookie popup
+      cookiePopup.style.display = 'none';
+  });
 
-// remove the cookie pop-up after a certain amount of time
-setTimeout(removePopup, popupTimeout);
+})();
 
-// Get the cookie popup element
-const cookiePopup = document.getElementById('cookie-popup');
-// Get the cookie accept button
-const cookieAccept = document.getElementById('cookie-accept');
-
-// Check if the cookie is set
-if (getCookie('cookie-accepted') !== 'true') {
-    // Show the cookie popup
-    cookiePopup.style.display = 'none';
-}
-
-// Add a click event listener to the cookie accept button
-cookieAccept.addEventListener('click', function() {
-    // Set the cookie to expire in 365 days
-    setCookie('cookie-accepted', 'true', 365);
-
-    // Hide the cookie popup
-    cookiePopup.style.display = 'block';
-});
 // Set a cookie with the given name, value and number of days until it expires
 function setCookie(name, value, days) {
     const date = new Date();
