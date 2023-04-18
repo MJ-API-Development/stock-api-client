d9ad529e3e2d9403c2bb26e2aba0348ba1d5cb8d
freelancing-solutions <mobiusndou@gmail.com>
2023-04-18 08:34:10 +0200

    Started on Flow for Google Auth

--- src/routes/authentication/dance.py
+++ src/routes/authentication/dance.py
@@ -10,8 +10,8 @@ app.secret_key = config_instance().SECRET_KEY
 google_dance = make_google_blueprint(client_id=config_instance().GOOGLE_SETTINGS.GOOGLE_CLIENT_ID,
                                      client_secret=config_instance().GOOGLE_SETTINGS.GOOGLE_CLIENT_SECRET,
                                      redirect_url="https://eod-stock-api.site/account",
-                                     scope=["/auth/userinfo.email",
-                                            "/auth/userinfo.profile",
+                                     scope=["https://www.googleapis.com/auth/userinfo.email",
+                                            "https://www.googleapis.com/auth/userinfo.profile",
                                             "openid"])
 
 
