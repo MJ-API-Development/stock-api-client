4c3efa7c3b119e0a47b289acd67c1330b63dac96
freelancing-solutions <mobiusndou@gmail.com>
2023-04-18 09:34:14 +0200

    Started on Flow for Google Auth

--- src/routes/authentication/dance.py
+++ src/routes/authentication/dance.py
@@ -9,7 +9,7 @@ app.secret_key = config_instance().SECRET_KEY
 
 google_dance = make_google_blueprint(client_id=config_instance().GOOGLE_SETTINGS.GOOGLE_CLIENT_ID,
                                      client_secret=config_instance().GOOGLE_SETTINGS.GOOGLE_CLIENT_SECRET,
-                                     redirect_url="/authorized",
+                                     redirect_url="/google/authorized",
                                      scope=["https://www.googleapis.com/auth/userinfo.email",
                                             "https://www.googleapis.com/auth/userinfo.profile",
                                             "openid"])
